a39c6e0e2e8b35f7c8f69e49c85f6892e427bf6c
ShrideviRShetty <158450109+ShrideviRShetty@users.noreply.github.com>
2024-04-15 12:19:16 +0530

    Create autoScalingGroup.tf

---
+++ autoScalingGroup.tf
@@ -0,0 +1,16 @@
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  min_size             = 2
+  max_size             = 3
+  desired_capacity     = 2
+  target_group_arns    = [aws_lb_target_group.example.arn]
+  vpc_zone_identifier  = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+
+  depends_on = [aws_lb_target_group.example]
+}
