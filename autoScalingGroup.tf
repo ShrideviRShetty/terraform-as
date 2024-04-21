d75ff9ecd68ded1ef04c2f0f42b35a759cb2b9d3
ShrideviRShetty <158450109+ShrideviRShetty@users.noreply.github.com>
2024-04-21 10:23:44 +0530

    Update autoScalingGroup.tf

--- autoScalingGroup.tf
+++ autoScalingGroup.tf
@@ -11,6 +11,4 @@ resource "aws_autoscaling_group" "example" {
     value               = "terraform-asg-example"
     propagate_at_launch = true
   }
-
-  depends_on = [aws_lb_target_group.example]
 }
