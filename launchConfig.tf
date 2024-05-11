ee8f94edec6200ca82adf01ee4a1642f1214fdc7
ShrideviRShetty <158450109+ShrideviRShetty@users.noreply.github.com>
2024-05-11 13:31:23 +0530

    Update launchConfig.tf

--- launchConfig.tf
+++ launchConfig.tf
@@ -1,8 +1,8 @@
 resource "aws_launch_configuration" "example" {
-  version         = v1.1
-  ami             = ami-0e4fd655fb4e26c30
-  instance_type   = aws_instance.example.instance_type
-  key_name        = aws_instance.example.key_name
+  version         = "v1.1"
+  ami             = "ami-0e4fd655fb4e26c30"
+  instance_type   = "t2.micro"
+  key_name        = "simple"
   user_data       = file("datafile.sh")
   security_groups = [aws_security_group.example.id]
 }
