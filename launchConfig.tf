82453870a3892f301eaf3681674b92836e58582b
ShrideviRShetty <158450109+ShrideviRShetty@users.noreply.github.com>
2024-05-11 13:29:41 +0530

    Update launchConfig.tf

--- launchConfig.tf
+++ launchConfig.tf
@@ -1,5 +1,6 @@
 resource "aws_launch_configuration" "example" {
-  image_id        = aws_instance.example.ami
+  version         = v1.1
+  ami             = ami-0e4fd655fb4e26c30
   instance_type   = aws_instance.example.instance_type
   key_name        = aws_instance.example.key_name
   user_data       = file("datafile.sh")
