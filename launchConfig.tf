fe51f337b634f71bea9cc9aa54a76118daefe6b9
ShrideviRShetty <158450109+ShrideviRShetty@users.noreply.github.com>
2024-05-11 13:32:41 +0530

    Update launchConfig.tf

--- launchConfig.tf
+++ launchConfig.tf
@@ -1,8 +1,11 @@
-resource "aws_launch_configuration" "example" {
+resource "aws_launch_template" "example" {
   version         = "v1.1"
   ami             = "ami-0e4fd655fb4e26c30"
   instance_type   = "t2.micro"
   key_name        = "simple"
   user_data       = file("datafile.sh")
   security_groups = [aws_security_group.example.id]
+tage={
+ name="Template1"
+}
 }
